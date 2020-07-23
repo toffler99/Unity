57dcc646ebc04054612554dfd4d1db87ac28c8f3
Dongwoo Pae <dongwoopae@gmail.com>
2020-07-22 22:43:26 -0500

    added a button in admin view

--- UnitySearch/View Controllers/AdminViewController.swift
+++ UnitySearch/View Controllers/AdminViewController.swift
@@ -13,6 +13,7 @@ class AdminViewController : UIViewController {
     
     //property
     var backBtn : UIImageView = UIImageView()
+    var sendButton: UIButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,8 @@ class AdminViewController : UIViewController {
         self.navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.005039108917, green: 0.2046912909, blue: 0.4367187917, alpha: 1)]
         self.navigationItem.hidesBackButton = true
         self.view.backgroundColor = .white
+        
+        self.setUpButton()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,4 +36,40 @@ class AdminViewController : UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
     
     }
+    
+    private func setUpButton() {
+        self.view.addSubview(sendButton)
+    
+        sendButton.backgroundColor = UIColor.orange
+        sendButton.tintColor = UIColor.white
+        sendButton.setTitle("Send Report", for: .normal)
+        
+        sendButton.translatesAutoresizingMaskIntoConstraints = false
+        sendButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100).isActive = true
+        sendButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        sendButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100).isActive = true
+        sendButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 300).isActive = true
+        
+        sendButton.isUserInteractionEnabled = true
+        sendButton.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
+    }
+    
+    @objc func buttonAction(sender: UIButton) {
+        print("button clicked!!!")
+        
+        
+        //add json into excel file (csv) and send it to recruiter's email
+        
+        
+        
+        
+        //add alert to tell the user that the report has been sent to the email
+        
+        
+        
+        
+    }
+    
+    
+    
 }
